e0e6b9a9bc6655430422dcbbeab881ed570d1952
evgalp <evgeniyalpayev@gmail.com>
2017-09-18 18:09:56 +0300

    particle gravity added

--- js/particlesFirework.js
+++ js/particlesFirework.js
@@ -3,27 +3,27 @@ window.onload = function() {
 		context = canvas.getContext("2d"),
 		width = canvas.width = window.innerWidth,
 		height = canvas.height = window.innerHeight,
-    particles = [],
-    numParticles = 100;
+		particles = [],
+		numParticles = 100;
 
-  for (var i = 0; i < numParticles; i+=1) {
-    particles.push(particle.create(width / 2, height / 2, Math.random() * 4 + 1, Math.random() * Math.PI * 2));
-  }
+	for(var i = 0; i < numParticles; i += 1) {
+		particles.push(particle.create(width / 2, height / 3, Math.random() * 5 + 2, Math.random() * Math.PI * 2, 0.1));
+	}
 
-  update();
+	update();
 
-  	function update() {
-  		context.clearRect(0, 0, width, height);
+	function update() {
+		context.clearRect(0, 0, width, height);
 
-  		for(var i = 0; i < numParticles; i += 1) {
-  			var p = particles[i];
-  			p.update();
+		for(var i = 0; i < numParticles; i += 1) {
+			var p = particles[i];
 
-  			context.beginPath();
-  			context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false);
-  			context.fill();
-  		}
-  		requestAnimationFrame(update);
-  	}
+			p.update();
 
+			context.beginPath();
+			context.arc(p.position.getX(), p.position.getY(), 4, 0, Math.PI * 2, false);
+			context.fill();
+		}
+		requestAnimationFrame(update);
+	}
 };
